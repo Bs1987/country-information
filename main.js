9c9256a0358af19d3ef18f536b6e0b973538bb49
Bs1987 <boysax@gmail.com>
2021-05-16 18:06:42 +0200

    Get API data on Belgium by pressing Enter in the input field

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ searchButton.addEventListener("click", async () => {
         "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
     );
     console.log(response.data);
-});
+
 
 const countryFlag = document.getElementById("countryFlag");
 console.log(countryFlag);
@@ -18,3 +18,19 @@ flagImage.src = "https://restcountries.eu/data/bel.svg";
 console.log(flagImage);
 countryFlag.appendChild(flagImage);
 console.log(flagImage);
+
+});
+
+
+const searchCountry = document.getElementById("searchCountry");
+console.log(searchCountry);
+
+searchCountry.addEventListener("keyup", async e => {
+    if ('Enter' === e.code) {
+        console.log("THIS IS INPUT ON ENTER")
+        const response = await axios.get(
+            "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
+        );
+        console.log(response.data);
+    }
+}, false);
