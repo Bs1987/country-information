1c390c7de50a0ce4eba0b96fddd943c4484b7e1f
Bs1987 <boysax@gmail.com>
2021-05-16 18:23:47 +0200

    added query, not quite working

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 const searchButton = document.getElementById("searchButton");
 
 searchButton.addEventListener("click", async () => {
-
+    const inputField = document.getElementById("searchCountry");
+    const query = inputField.value;
+    console.log(query);
     // Select input
     //Take value from input
     const response = await axios.get(
-        "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
+        `https://restcountries.eu/rest/v2/name/${ query }` //<-- replace value here
     );
     console.log(response.data);
 
 
 const countryFlag = document.getElementById("countryFlag");
-console.log(countryFlag);
+// console.log(countryFlag);
 const flagImage = document.createElement("img");
-console.log(flagImage);
+// console.log(flagImage);
 flagImage.src = "https://restcountries.eu/data/bel.svg";
-console.log(flagImage);
+// console.log(flagImage);
 countryFlag.appendChild(flagImage);
-console.log(flagImage);
+// console.log(flagImage);
 
 });
 
 
 const searchCountry = document.getElementById("searchCountry");
-console.log(searchCountry);
+// console.log(searchCountry);
 
 searchCountry.addEventListener("keyup", async e => {
     if ('Enter' === e.code) {
-        console.log("THIS IS INPUT ON ENTER")
+        // console.log("THIS IS INPUT ON ENTER")
         const response = await axios.get(
-            "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
+            `https://restcountries.eu/rest/v2/name/${query}` //<-- replace value here
         );
-        console.log(response.data);
+        // console.log(response.data);
     }
+
+
+
 }, false);
