dbc0e0fa057413e87af26cb88de5146d91c1d618
Bs1987 <boysax@gmail.com>
2021-05-16 17:33:58 +0200

    make todolist

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const searchButton = document.getElementById("searchButton");
 
 searchButton.addEventListener("click", async () => {
+
+    // Select input
+    //Take value from input
     const response = await axios.get(
-        "https://restcountries.eu/rest/v2/name/belgium"
+        "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
     );
     console.log(response);
 });
