0fcdfd58687723ce0372ef734bc7313184fc9e9c
Bs1987 <boysax@gmail.com>
2021-05-16 17:42:51 +0200

    Made flag appear on page

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ searchButton.addEventListener("click", async () => {
     const response = await axios.get(
         "https://restcountries.eu/rest/v2/name/belgium" //<-- replace value here
     );
-    console.log(response);
+    console.log(response.data);
 });
 
-
+const countryFlag = document.getElementById("countryFlag");
+console.log(countryFlag);
+const flagImage = document.createElement("img");
+console.log(flagImage);
+flagImage.src = "https://restcountries.eu/data/bel.svg";
+console.log(flagImage);
+countryFlag.appendChild(flagImage);
+console.log(flagImage);
